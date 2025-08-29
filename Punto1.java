4dbca9f5a787c6aff4ed15c5cc92a13a742f6260
unknown <tuemail@gmail.com>
2025-08-29 19:06:46 -0300

    Avance del punto 1 del Trabajo Practico N°5

--- Punto1.java
+++ Punto1.java
@@ -2,24 +2,28 @@
 public class Punto1 {
 public static void main(String[] args) {
  String nombreDePais = "argentina"; 
- Integer edadPersona = 42; 
- double calculoCoseno 
-
-
-
-Integer num1 = 8; 
-Integer num2 = 5;
-Integer num3 = 1;
-Integer num4 = 20;
-Integer num5 = 9;
-Integer suma = num1+num2+num3+num4+num5; 
+ int edadPersona = 42; 
+ Double alturaEdificio = 123.55; 
+ Double precioProductoSupermercado = 245.45; 
+ String telefono = "3884651866"; 
+ double calculoCoseno = Math.cos(0.5); 
+int num1 = 8; 
+int num2 = 5;
+int num3 = 1;
+int num4 = 20;
+int num5 = 9;
+int suma = num1+num2+num3+num4+num5; 
 Double promedio =   (double) (suma)/(5); 
 
+// Escribir las condiciones
 
 System.out.println(nombreDePais);
-System.out.println(promedio );
-
-
+System.out.println(edadPersona);
+System.out.println(alturaEdificio );
+System.out.println(precioProductoSupermercado);
+System.out.println(telefono );
+System.out.println(calculoCoseno );
+System.out.println(promedio);
 }                                                         
 
 }
