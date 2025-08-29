09cd944eb92553a60ec80560d753d30b7767afba
unknown <tuemail@gmail.com>
2025-08-28 21:51:53 -0300

    comenzando el punto 1 del TP5

---
+++ Punto1.java
@@ -0,0 +1,25 @@
+// package Punto1; 
+public class Punto1 {
+public static void main(String[] args) {
+ String nombreDePais = "argentina"; 
+ Integer edadPersona = 42; 
+ double calculoCoseno 
+
+
+
+Integer num1 = 8; 
+Integer num2 = 5;
+Integer num3 = 1;
+Integer num4 = 20;
+Integer num5 = 9;
+Integer suma = num1+num2+num3+num4+num5; 
+Double promedio =   (double) (suma)/(5); 
+
+
+System.out.println(nombreDePais);
+System.out.println(promedio );
+
+
+}                                                         
+
+}
