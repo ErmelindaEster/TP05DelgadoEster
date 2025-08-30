37f681fc5492205d7ba5ece3554749b36fccbf39
unknown <tuemail@gmail.com>
2025-08-29 22:24:59 -0300

    desarrollo del ejercicio01, del T.P 5

--- ejercicio01/Main.java
+++ ejercicio01/Main.java
@@ -1,5 +1,6 @@
-// package Punto1; 
-public class Punto1 {
+package ejercicio01;
+// pakege ejercicio01
+public class Main {
 public static void main(String[] args) {
  String nombreDePais = "argentina"; 
  int edadPersona = 42; 
