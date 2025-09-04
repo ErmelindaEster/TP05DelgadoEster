6ce29835be9c2d8b40992b17edcf9e19ef9ed37d
unknown <tuemail@gmail.com>
2025-09-03 21:15:36 -0300

    terminando el ejercicico 2 del TP5

--- ejercicio02/Main.java
+++ ejercicio02/Main.java
@@ -1,5 +1,19 @@
 package ejercicio02;
 
+import java.util.Scanner;
+
 public class Main {
-    
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Ingrese un numero entero:");
+    int numero = scanner.nextInt();
+    if (numero % 2 == 0){
+      System.out.println("El numero es par.Eltriple es:" +(numero*3));
+    } else{
+       System.out.println("El numero es impar.El doble es: " + (numero*2));
+    }
+    scanner.close();
+    }
+
 }
