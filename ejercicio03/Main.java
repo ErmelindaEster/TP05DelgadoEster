c41c7dfe4b167f93b5959cca86efd5598fc84ba6
unknown <tuemail@gmail.com>
2025-09-05 23:56:11 -0300

    Ejercicio N°3 del TP 5, terminado

--- ejercicio03/Main.java
+++ ejercicio03/Main.java
@@ -1,7 +1,6 @@
 package ejercicio03;
 import java.util.Scanner;
 public class Main {
-
   public static void main(String[] args) {
       Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese un numero entre 0 y 10: "); 
