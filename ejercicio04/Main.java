658c66f279bbc12dbb930f6ec7d4758a05a9056d
unknown <tuemail@gmail.com>
2025-09-06 22:21:34 -0300

    Ejercicio N°4 del TP5 terminado

--- ejercicio04/Main.java
+++ ejercicio04/Main.java
@@ -1,6 +1,16 @@
 package ejercicio04;
-
+import java.util.Scanner;
 public class Main {
-    
+      public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un numero entre 1 y 9: ");
+        int numero = scanner.nextInt(); 
+        System.out.println(" Tabla de multiplicar del " + numero + ":"); 
+             for (int i=1; i <=10; i++) {
+             System.out.println( numero + " x " + i + " = " + (numero * i)); 
+
+      }
+      scanner. close(); 
+      }
     
 }
--- ejercicio05/Main.java
+++ ejercicio05/Main.java
@@ -1,5 +1,8 @@
 package ejercicio05;
-
+import java.util.Scanner;
 public class Main {
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
 
 }
